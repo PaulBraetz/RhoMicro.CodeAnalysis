acd510673d8bc3edc7c33077ae8c286db232629a
Paul Braetz <paul.braetz@rhomicro.com>
2024-02-15 00:57:36 +0100

    fix env dependency on newlines

--- DslGenerator.Tests/TokenizerTests.cs
+++ DslGenerator.Tests/TokenizerTests.cs
@@ -9,57 +9,66 @@ using System.Diagnostics;
 
 public class TokenizerTests
 {
-    [Theory(Timeout = 5000)]
-    [InlineData("name=name", new[] { "Name:name", "Equal:=", "Name:name" }, new Int32[] { })]
-    [InlineData("name=\"\";", new[] { "Name:name", "Equal:=", "Terminal:", "Semicolon:;" }, new Int32[] { })]
-    [InlineData("name=\"a\"-\"z\";", new[] { "Name:name", "Equal:=", "Terminal:a", "Dash:-", "Terminal:z", "Semicolon:;" }, new Int32[] { })]
-    [InlineData("name", new[] { "Name:name" }, new Int32[] { })]
-    [InlineData("=", new[] { "Equal:=" }, new Int32[] { })]
-    [InlineData(".", new[] { "Period:." }, new Int32[] { })]
-    [InlineData("TerminalValue = \"\\\"\" . \"\\\"\";",
-        new[] { "Name:TerminalValue", "Whitespace: ", "Equal:=", "Whitespace: ", "Terminal:\\\"", "Whitespace: ", "Period:.", "Whitespace: ", "Terminal:\\\"","Semicolon:;" }, new Int32[] { })]
-    [InlineData("-", new[] { "Dash:-" }, new Int32[] { })]
-    [InlineData("\"a\"-\"z\"", new[] { "Terminal:a", "Dash:-", "Terminal:z" }, new Int32[] { })]
-    [InlineData("\"ab\"-\"z\"", new[] { "Terminal:ab", "Dash:-", "Terminal:z" }, new Int32[] { })]
-    [InlineData("     ", new[] { "Whitespace:     " }, new Int32[] { })]
-    [InlineData(" ", new[] { "Whitespace: " }, new Int32[] { })]
-    [InlineData("\t", new[] { "Whitespace:\t" }, new Int32[] { })]
-    [InlineData("\t\t\t", new[] { "Whitespace:\t\t\t" }, new Int32[] { })]
-    [InlineData("\n", new[] { "NewLine:\n" }, new Int32[] { })]
-    [InlineData("\n\n\n", new[] { "NewLine:\n", "NewLine:\n", "NewLine:\n" }, new Int32[] { })]
-    [InlineData("\r", new[] { "NewLine:\r" }, new Int32[] { })]
-    [InlineData("\r\r\r", new[] { "NewLine:\r", "NewLine:\r", "NewLine:\r" }, new Int32[] { })]
-    [InlineData("\r\n", new[] { "NewLine:\r\n" }, new Int32[] { })]
-    [InlineData("\r\n\r\n\r\n", new[] { "NewLine:\r\n", "NewLine:\r\n", "NewLine:\r\n" }, new Int32[] { })]
-    [InlineData("\r\n\n\r\n", new[] { "NewLine:\r\n", "NewLine:\n", "NewLine:\r\n" }, new Int32[] { })]
-    [InlineData(";", new[] { "Semicolon:;" }, new Int32[] { })]
-    [InlineData("\"", new String[] { }, new Int32[] { })]
-    [InlineData("/", new[] { "Slash:/" }, new Int32[] { })]
-    [InlineData("#", new String[] { }, new Int32[] { })]
-    [InlineData("# this is a comment; 24224 ßßßß", new[] { "Comment: this is a comment; 24224 ßßßß" }, new Int32[] { })]
-    [InlineData("/=", new[] { "SlashEqual:/=" }, new Int32[] { })]
-    [InlineData("(", new[] { "ParenLeft:(" }, new Int32[] { })]
-    [InlineData(")", new[] { "ParenRight:)" }, new Int32[] { })]
-    [InlineData("[", new[] { "BracketLeft:[" }, new Int32[] { })]
-    [InlineData("]", new[] { "BracketRight:]" }, new Int32[] { })]
-    [InlineData("32rule", new[] { "Number:32", "Name:rule" }, new Int32[] { })]
-    [InlineData("&33+", new[] { "Unknown:&", "Number:33", "Unknown:+" }, new Int32[] { })]
-    [InlineData("%%%", new[] { "Unknown:%%%" }, new[] { 1 })]
-    [InlineData("\"TerminalValue\"", new[] { "Terminal:TerminalValue" }, new Int32[] { })]
-    [InlineData("\"TerminalValue", new[] { "Terminal:TerminalValue" }, new[] { 2 })]
-    [InlineData("\"TerminalValue;rule=somename;", new[] { "Terminal:TerminalValue;rule=somename;" }, new[] { 2 })]
-    [InlineData("\"TerminalValue;ru\\\"le=somename;\"", new[] { "Terminal:TerminalValue;ru\\\"le=somename;" }, new Int32[] { })]
-    [InlineData("""
+#pragma warning disable RS1035 // Do not use APIs banned for analyzers
+    public static Object[][] TokenizerData => [
+        ["name=name", new[] { "Name:name", "Equal:=", "Name:name" }, Array.Empty<Int32>()],
+        ["name=\"\";", new[] { "Name:name", "Equal:=", "Terminal:", "Semicolon:;" }, Array.Empty<Int32>()],
+        ["name=\"a\"-\"z\";", new[] { "Name:name", "Equal:=", "Terminal:a", "Dash:-", "Terminal:z", "Semicolon:;" }, Array.Empty<Int32>()],
+        ["name", new[] { "Name:name" }, Array.Empty<Int32>()],
+        ["=", new[] { "Equal:=" }, Array.Empty<Int32>()],
+        [".", new[] { "Period:." }, Array.Empty<Int32>()],
+        ["TerminalValue = \"\\\"\" . \"\\\"\";",
+            new[] { "Name:TerminalValue", "Whitespace: ", "Equal:=", "Whitespace: ", "Terminal:\\\"", "Whitespace: ", "Period:.", "Whitespace: ", "Terminal:\\\"", "Semicolon:;" },
+            Array.Empty<Int32>()],
+        ["-", new[] { "Dash:-" }, Array.Empty<Int32>()],
+        ["\"a\"-\"z\"", new[] { "Terminal:a", "Dash:-", "Terminal:z" }, Array.Empty<Int32>()],
+        ["\"ab\"-\"z\"", new[] { "Terminal:ab", "Dash:-", "Terminal:z" }, Array.Empty<Int32>()],
+        ["     ", new[] { "Whitespace:     " }, Array.Empty<Int32>()],
+        [" ", new[] { "Whitespace: " }, Array.Empty<Int32>()],
+        ["\t", new[] { "Whitespace:\t" }, Array.Empty<Int32>()],
+        ["\t\t\t", new[] { "Whitespace:\t\t\t" }, Array.Empty<Int32>()],
+        ["\n", new[] { "NewLine:\n" }, Array.Empty<Int32>()],
+        ["\n\n\n", new[] { "NewLine:\n", "NewLine:\n", "NewLine:\n" }, Array.Empty<Int32>()],
+        ["\r", new[] { "NewLine:\r" }, Array.Empty<Int32>()],
+        ["\r\r\r", new[] { "NewLine:\r", "NewLine:\r", "NewLine:\r" }, Array.Empty<Int32>()],
+        ["\r\n", new[] { "NewLine:\r\n" }, Array.Empty<Int32>()],
+        ["\r\n\r\n\r\n", new[] { "NewLine:\r\n", "NewLine:\r\n", "NewLine:\r\n" }, Array.Empty<Int32>()],
+        ["\r\n\n\r\n", new[] { "NewLine:\r\n", "NewLine:\n", "NewLine:\r\n" }, Array.Empty<Int32>()],
+        [";", new[] { "Semicolon:;" }, Array.Empty<Int32>()],
+        ["\"", Array.Empty<String>(), Array.Empty<Int32>()],
+        ["/", new[] { "Slash:/" }, Array.Empty<Int32>()],
+        ["#", Array.Empty<String>(), Array.Empty<Int32>()],
+        ["# this is a comment; 24224 ßßßß", new[] { "Comment: this is a comment; 24224 ßßßß" }, Array.Empty<Int32>()],
+        ["/=", new[] { "SlashEqual:/=" }, Array.Empty<Int32>()],
+        ["(", new[] { "ParenLeft:(" }, Array.Empty<Int32>()],
+        [")", new[] { "ParenRight:)" }, Array.Empty<Int32>()],
+        ["[", new[] { "BracketLeft:[" }, Array.Empty<Int32>()],
+        ["]", new[] { "BracketRight:]" }, Array.Empty<Int32>()],
+        ["32rule", new[] { "Number:32", "Name:rule" }, Array.Empty<Int32>()],
+        ["&33+", new[] { "Unknown:&", "Number:33", "Unknown:+" }, Array.Empty<Int32>()],
+        ["%%%", new[] { "Unknown:%%%" }, new[] { 1 }],
+        ["\"TerminalValue\"", new[] { "Terminal:TerminalValue" }, Array.Empty<Int32>()],
+        ["\"TerminalValue", new[] { "Terminal:TerminalValue" }, new[] { 2 }],
+        ["\"TerminalValue;rule=somename;", new[] { "Terminal:TerminalValue;rule=somename;" }, new[] { 2 }],
+        ["\"TerminalValue;ru\\\"le=somename;\"", new[] { "Terminal:TerminalValue;ru\\\"le=somename;" }, Array.Empty<Int32>()],
+        [       """
                 testname = ["is this the 1 legal value?"] / # here is a comment
                     *"no, it's not :)"; # and another comment
                 
-                """, new[] { "Name:testname", "Whitespace: ", "Equal:=", "Whitespace: ",
+                """,
+            new[] { "Name:testname", "Whitespace: ", "Equal:=", "Whitespace: ",
                              "BracketLeft:[", "Terminal:is this the 1 legal value?", "BracketRight:]",
-                             "Whitespace: ","Slash:/","Whitespace: ", "Comment: here is a comment", "NewLine:\r\n",
+                             "Whitespace: ","Slash:/","Whitespace: ", "Comment: here is a comment", $"NewLine:{Environment.NewLine}",
                              "Whitespace:    ","Star:*","Terminal:no, it's not :)" ,
-                             "Semicolon:;", "Whitespace: ", "Comment: and another comment", "NewLine:\r\n",
-                             }, new Int32[] { })]
-    [InlineData("a=b;a/=c;", new[] { "Name:a", "Equal:=", "Name:b", "Semicolon:;", "Name:a", "SlashEqual:/=", "Name:c", "Semicolon:;" }, new Int32[] { })]
+                             "Semicolon:;", "Whitespace: ", "Comment: and another comment", $"NewLine:{Environment.NewLine}",
+                             },
+            Array.Empty<Int32>()],
+        ["a=b;a/=c;", new[] { "Name:a", "Equal:=", "Name:b", "Semicolon:;", "Name:a", "SlashEqual:/=", "Name:c", "Semicolon:;" }, Array.Empty<Int32>()]
+    ];
+#pragma warning restore RS1035 // Do not use APIs banned for analyzers
+
+    [Theory(Timeout = 5000)]
+    [MemberData(nameof(TokenizerData))]
     public void TokenizesCorrectly(String source, String[] rawTokens, Int32[] expectedDiagnosticIds)
     {
         //Arrange
