1d6eb098d40dc667f5dc41f4ac9a4868dd62b59f
Paul Braetz <paul.braetz@rhomicro.com>
2024-02-28 18:00:01 +0100

    add license option

--- UtilityGenerators.Library/Text/IndentedStringBuilderOptions.cs
+++ UtilityGenerators.Library/Text/IndentedStringBuilderOptions.cs
@@ -8,13 +8,22 @@ partial record IndentedStringBuilderOptions
     public Boolean PrependMarkerComment { get; init; } = false;
     public Boolean PrependWarningDisablePragma { get; init; } = false;
     public Boolean PrependNullableEnable { get; init; } = false;
+    public String License { get; init; } = String.Empty;
     public CancellationToken AmbientCancellationToken { get; init; } = CancellationToken.None;
+
+    private const String _defaultLicense = 
+        """
+        // The tool used to generate this code may be subject to license terms;
+        // this generated code is however not subject to those terms, instead it is
+        // subject to the license (if any) applied to the containing project.
+        """;
 
     public static IndentedStringBuilderOptions GeneratedFile = new()
     {
         PrependMarkerComment = true,
         PrependWarningDisablePragma = true,
-        PrependNullableEnable = true
+        PrependNullableEnable = true,
+        License = _defaultLicense
     };
     public static IndentedStringBuilderOptions Default = new();
 }
